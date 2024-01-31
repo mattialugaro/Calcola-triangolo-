610f50d23db54bd38bc623559c1be1fc4fa26615
mattia lugaro <mattialugaro@gmail.com>
2024-01-31 09:28:13 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scrivi la lunghezza lato1 del trinagolo...");
+            int lato1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Scrivi la lunghezza lato2 del trinagolo...");
+            int lato2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Scrivi la lunghezza lato3 del trinagolo...");
+            int lato3 = Convert.ToInt16(Console.ReadLine());
+
+            Triangolo triangolo1 = new Triangolo(lato1, lato2, lato3);
+            string messaggio = triangolo1.checkTriangolo();
+            Console.WriteLine(messaggio);
+            Console.ReadLine();
+
+            string messaggio2 = triangolo1.latoPiuLungo();
+            Console.WriteLine(messaggio2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Triangolo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Triangolo
+    {
+        public int lato1 {  get; set; }
+        public int lato2 { get; set; }
+        public int lato3 { get; set;}
+
+        public string checkTriangolo()
+        {
+            if(lato1 == lato2 && lato2 == lato3)
+            {
+                return "Il triangolo e' equilatero";
+            }
+            else if(lato1 == lato2 || lato1 == lato3 || lato2 == lato3)
+            {
+                return "Il triangolo e' isoscele";
+            }
+            else
+            {
+                return "Il triangolo e' scaleno";
+            }
+        }
+
+        public Triangolo(int lato1, int lato2, int lato3)
+        {
+            this.lato1 = lato1;
+            this.lato2 = lato2;
+            this.lato3 = lato3;
+        }
+
+        public string latoPiuLungo()
+        {
+            if (lato1 > lato2 && lato1 > lato3)
+            {
+                return "Il lato piu' lungo e' " + lato1;
+            }
+            else if (lato2 > lato3 && lato2 > lato1)
+            {
+                return "Il lato piu' lungo e' " + lato2;
+            }
+            else
+            {
+                return "Il lato piu' lungo e' " + lato3;
+            }
+        }
+    }
+}
